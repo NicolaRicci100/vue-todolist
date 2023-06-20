e406d77b572c4e524e3440ccfb59fed7894df048
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-06-20 15:56:45 +0200

    v-for per la lista

--- js/script.js
+++ js/script.js
@@ -6,7 +6,24 @@ const { createApp } = Vue;
 createApp({
     data(){
         return{
-            
+            actionList:[
+                {
+                    text: 'Dormire',
+                    done: false
+                },
+                {
+                    text: 'Alzarsi',
+                    done: false
+                },
+                {
+                    text: 'Bere',
+                    done: false
+                },
+                {
+                    text: 'Mangiare',
+                    done: false
+                },
+            ]
         }
     }
 }).mount('#root');
