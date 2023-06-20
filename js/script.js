96ac209789f731a562debc88cf3a36abac6bae25
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-06-20 17:58:04 +0200

    aggiunta chiave numeri all'array

--- js/script.js
+++ js/script.js
@@ -8,19 +8,23 @@ createApp({
         return{
             newCard:'',
             deck:[
-                {
+                {   
+                    number: 1, // numeri aggiunti per gestire meglio il metodo *toggleCardStatus*
                     text: 'Black Lotus',
                     done: false
                 },
                 {
+                    number: 2,
                     text: 'Ancestral Recall',
                     done: false
                 },
                 {
+                    number: 3,
                     text: 'Time Walk',
                     done: false
                 },
                 {
+                    number: 4,
                     text: 'Timetwister',
                     done: false
                 },
@@ -28,10 +32,21 @@ createApp({
         }
     },
 
+    computed:{
+
+
+    },
+
     methods:{
         // metodo per togliere una carta
         removeCard(cardSelected){
             this.deck.splice(cardSelected, 1);
+        },
+        // metodo per "tagliare" il testo di una carta
+        toggleCardStatus(cardSigned){
+            this.deck.forEach(card => {
+                if(card.number === cardSigned) card.done = !card.done;
+            });
         }
     }
 }).mount('#root');
